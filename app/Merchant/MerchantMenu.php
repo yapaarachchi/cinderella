b1dac844f33bb466705d3193b40fd4731be02918
Gayan Yapaarachchi <gayan.yapaarachchi@ifsworld.com>
2017-04-13 20:53:07 +0530

    sidemenu

--- app/Merchant/MerchantMenu.php
+++ app/Merchant/MerchantMenu.php
@@ -21,7 +21,14 @@ $Business = new \Delight\Auth\Business($db);
 <?php
 foreach($Business->getBusinessByUserId($auth->getUserId()) as $key => $value) {
 ?>
- <?php echo '<a href="#" style="font-size: 0.90rem; padding: 0.50rem;" class="list-group-item list-group-item-action" id="'.$value['id'].'">'.$value['business_name'].'</a>';?>
+ <?php 
+ $text = '<a href="#" style="font-size: 0.90rem; padding: 0.50rem;" class="list-group-item list-group-item-action justify-content-between" id="'.$value['id'].'">'.$value['business_name'];
+ if($Business->getBusinessStatus($value['id']) == 'NOT_APPROVE'){
+	$text = $text. '<span class="badge badge-default badge-pill">!</span>';
+ }
+ $text = $text. '</a>';
+ echo $text;
+ ?>
 				
 <?php
 }
